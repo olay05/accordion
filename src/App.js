bfbdd50dd7bf972591878e5adc725e037fca4019
olay05 <olayinkami05@gmail.com>
2023-05-17 15:11:02 +0100

    new accordion with react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./index.css";
+import Header from "./Header";
+import FAQ from "./FAQ";
 
-function App() {
+export default function App() {
+  const [faqs, setFaqs] = useState([
+    {
+      question: "How many programmers does it take to screw a lightbulb?",
+      answer:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed pharetra lorem eu dolor rhoncus, at scelerisque ligula gravida. Sed porta id mi sit amet convallis. Etiam iaculis massa sit amet lacus blandit sodales. Nulla ultrices velit a diam placerat congue. Pellentesque iaculis, ipsum quis eleifend dapibus, est dui eleifend ante, quis fermentum mi ligula quis nisl. Ut et ex dui. Integer id venenatis quam.",
+      open: true
+    },
+    {
+      question: "Who is the most awesome person?",
+      answer: "You! The viewer!",
+      open: false
+    },
+    {
+      question:
+        "How many questions does it take to makes a succesful FAQ Page?",
+      answer: "This many!",
+      open: false
+    }
+  ]);
+
+  const toggleFAQ = index => {
+    setFaqs(
+      faqs.map((faq, i) => {
+        if (i === index) {
+          faq.open = !faq.open;
+        } else {
+          faq.open = false;
+        }
+
+        return faq;
+      })
+    );
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <div className="faqs">
+        {faqs.map((faq, index) => (
+          <FAQ faq={faq} index={index} key={index} toggleFAQ={toggleFAQ} />
+        ))}
+      </div>
     </div>
   );
 }
 
-export default App;
---
+++ src/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./index.css";
+
+const Header = () => {
+  return (
+    <header>
+      <h1> My FAQ Page</h1>
+    </header>
+  );
+};
+
+export default Header;
